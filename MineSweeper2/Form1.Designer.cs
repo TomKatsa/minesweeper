a8bc56f489852f62c0dd78f8e794d165d8a9f464
Tom <x>
2019-11-09 19:54:45 +0200

    Form renaming

--- MineSweeper2/Form1.Designer.cs
+++ MineSweeper2/Form1.Designer.cs
@@ -109,7 +109,7 @@
             this.Controls.Add(this.heightBox);
             this.Controls.Add(this.widthBox);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Minesweeper";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
